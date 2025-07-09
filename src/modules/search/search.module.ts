bbfdad70b8abdbe18554cc46a25163bcbf60cd22
MateusDonaire <canalmateusdonaire@gmail.com>
2025-07-09 02:19:45 -0300

    All topics

--- src/modules/search/search.module.ts
+++ src/modules/search/search.module.ts
@@ -4,10 +4,11 @@ import { SearchVersesUseCase } from './use-cases/search-verse.use-case';
 import { SearchController } from './controllers/search.controller';
 import { TopicsController } from './controllers/topic.controller';
 import { GetPopularTopicsUseCase } from './use-cases/get-popular-topics.use-case';
+import { GetAllTopicsUseCase } from './use-cases/get-all-topics.use-case';
 
 @Module({
   imports: [PrismaModule],
   controllers: [SearchController, TopicsController],
-  providers: [SearchVersesUseCase, GetPopularTopicsUseCase],
+  providers: [SearchVersesUseCase, GetPopularTopicsUseCase, GetAllTopicsUseCase],
 })
 export class SearchModule {}
