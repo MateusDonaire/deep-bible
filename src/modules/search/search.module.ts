355df10ed0c2d33990474ca7b34082f4d364978c
MateusDonaire <canalmateusdonaire@gmail.com>
2025-07-11 00:30:03 -0300

    Update search.module.ts

--- src/modules/search/search.module.ts
+++ src/modules/search/search.module.ts
@@ -5,10 +5,19 @@ import { SearchController } from './controllers/search.controller';
 import { TopicsController } from './controllers/topic.controller';
 import { GetPopularTopicsUseCase } from './use-cases/get-popular-topics.use-case';
 import { GetAllTopicsUseCase } from './use-cases/get-all-topics.use-case';
+import OpenAI from 'openai';
 
 @Module({
   imports: [PrismaModule],
   controllers: [SearchController, TopicsController],
-  providers: [SearchVersesUseCase, GetPopularTopicsUseCase, GetAllTopicsUseCase],
+  providers: [
+    SearchVersesUseCase,
+    GetPopularTopicsUseCase,
+    GetAllTopicsUseCase,
+    {
+      provide: OpenAI,
+      useFactory: () => new OpenAI({ apiKey: process.env.OPENAI_API_KEY }),
+    },
+  ],
 })
 export class SearchModule {}
